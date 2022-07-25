991260f7b2ed7423761418882e7a8ae9c9bb00b3
Jiamin Li <lijiamin411@qq.com>
2022-07-25 23:41:42 +0800

    关于静态任务服务的设计

--- blade-ops/blade-task/src/main/java/org/springblade/aop/impl/TaskThreadHandleImpl.java
+++ blade-ops/blade-task/src/main/java/org/springblade/aop/impl/TaskThreadHandleImpl.java
@@ -24,15 +24,30 @@ public class TaskThreadHandleImpl {
 	 */
 	@Around("@annotation(org.springblade.aop.TaskThreadHandle)")
 	public Object asyncThreadExecute(ProceedingJoinPoint point) throws Throwable {
+
+		// 思考了许久 任务的开始与结束应该有一个消息表去维护 就没必要在返回值做太多文章了
+		// id
+		// 任务名称 name
+		// 执行开始时间 start_time
+		// 执行结束时间 end_time
+		// 任务状态(0:执行完成,1:执行失败) status
+		// ...
+
 		CompletableFuture.runAsync(() -> {
 			try {
+				// 记录线程开始时间及结束时间
+				// ...
 				point.proceed();
 				// 这里应该套一层超时 以后再说
+				// ...
 			} catch (Throwable e) {
 				throw new RuntimeException(e);
+				// 如果抛出异常应该是任务执行失败
+				// ...
 			}
 		}, ThreadPoolUtils.getThreadPool());
-		// void 也不要返回值了
+
+		// 不要返回值了
 		return null;
 	}
 }
